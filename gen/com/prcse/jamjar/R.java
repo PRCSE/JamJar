c79be8e76cbc2f2ed164c4890f31714d47df5a1e
freebie <jamesstidard@gmail.com>
2013-03-29 19:35:35 +0000

    Image Adapter for Spotlight set

--- gen/com/prcse/jamjar/R.java
+++ gen/com/prcse/jamjar/R.java
@@ -32,11 +32,15 @@ public final class R {
         public static final int social_person=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int artist_venue_grid=0x7f080001;
         public static final int gridview=0x7f080000;
+        public static final int thumbnail=0x7f080002;
+        public static final int title=0x7f080003;
     }
     public static final class layout {
         public static final int activity_spotlight=0x7f030000;
+        public static final int artist_venue_tile=0x7f030001;
     }
     public static final class menu {
         public static final int spotlight=0x7f070000;
--- src/com/prcse/jamjar/GridAdapter.java
+++ src/com/prcse/jamjar/GridAdapter.java
@@ -1,24 +1,27 @@
 package com.prcse.jamjar;
 
+
 import java.util.ArrayList;
-
 import com.prcse.datamodel.Artist;
-
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class GridAdapter extends BaseAdapter {
 	private Context mContext;
+	private LayoutInflater layoutInflater;
 	private ArrayList artists = null;
 
     public GridAdapter(Context c) {
         mContext = c;
+        layoutInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
     
     public void setArtists(ArrayList artists) {
@@ -43,36 +46,37 @@ public class GridAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-    	TextView textView;
-    	Artist artist = (Artist) artists.get(position);
-    	
+
+    	ViewHolder holder;
     	
         ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+        if (convertView == null) {  // if it's not recycled, initialise some attributes
+            convertView = layoutInflater.inflate(R.layout.artist_venue_tile, parent, false);
+            
+            holder = new ViewHolder();
+        	holder.rl = (RelativeLayout) convertView.findViewById(R.id.artist_venue_grid);
+    		holder.image = (ImageView) convertView.findViewById(R.id.thumbnail);
+    		holder.text = (TextView) convertView.findViewById(R.id.title);
+    		
+    		convertView.setTag(holder);
+        	
+        	//imageView = new ImageView(mContext);
+            //imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            //imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //imageView.setPadding(8, 8, 8, 8);
         } else {
-            imageView = (ImageView) convertView;
+        	holder = (ViewHolder) convertView.getTag();
         }
 
+        Artist artist = (Artist) artists.get(position);
+        holder.text.setText(artist.getName());
         //imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        return convertView;
     }
-
-//    // references to our images
-//    private Integer[] mThumbIds = {
-//            R.drawable.sample_2, R.drawable.sample_3,
-//            R.drawable.sample_4, R.drawable.sample_5,
-//            R.drawable.sample_6, R.drawable.sample_7,
-//            R.drawable.sample_0, R.drawable.sample_1,
-//            R.drawable.sample_2, R.drawable.sample_3,
-//            R.drawable.sample_4, R.drawable.sample_5,
-//            R.drawable.sample_6, R.drawable.sample_7,
-//            R.drawable.sample_0, R.drawable.sample_1,
-//            R.drawable.sample_2, R.drawable.sample_3,
-//            R.drawable.sample_4, R.drawable.sample_5,
-//            R.drawable.sample_6, R.drawable.sample_7
-//    };
+    
+    static class ViewHolder {
+    	RelativeLayout rl;
+    	ImageView image;
+    	TextView text;
+    }
 }
